b3394c52312851457b0bce82bd421bc108e14205
Mikhail Goncharov <goncharov.mikhail@gmail.com>
2019-03-16 19:09:59 +0100

    bt commands

--- src/main.ino
+++ src/main.ino
@@ -8,6 +8,14 @@
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40);
 LiquidCrystal_PCF8574 lcd(0x27);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+const String rotate_x_command = "rx";
+const String rotate_y_command = "ry";
+int rotation_x_target = 0;
+int rotation_x = 0;
+int rotation_y_target = 0;
+int rotation_y = 0;
+
 void setup() {
     pinMode(LED_BUILTIN, OUTPUT);
     Serial.begin(9600);
@@ -24,8 +32,10 @@ void setup() {
 
   pwm.begin();
   pwm.setPWMFreq(60);
-  set_x_rotation(90);
-  set_y_rotation(10);
+  rotation_y_target = 40;
+  rotation_y = 0;
+  rotation_x_target = 0;
+  rotation_x = 1;
 }
 
 int line = 0;
@@ -43,37 +53,54 @@ void bt_loop() {
     bt_buffer += char(Serial.read());
   }  
   if (bt_buffer.endsWith(String("\r\n"))) {
-    lcd_line(bt_buffer.substring(0, bt_buffer.length()-2));
-    Serial.print(bt_buffer);
+    bt_buffer = bt_buffer.substring(0, bt_buffer.length()-2);
+    lcd_line(bt_buffer);
+    process_command(bt_buffer);
+    Serial.print(bt_buffer + "\r\n");
     bt_buffer = "";
   }
 }
 
+void process_command(String c) {
+  if (c.startsWith(rotate_x_command)) {
+    rotation_x_target = c.substring(rotate_x_command.length() + 1).toInt();
+  }
+  if (c.startsWith(rotate_y_command)) {
+    rotation_y_target = c.substring(rotate_y_command.length() + 1).toInt();
+  }
+}
+
 double deg = 0;
+
+
 void servo_loop() {
-  double d = deg;
-  if (d > 180) d = 360 - d;
-  set_x_rotation(d - 90);
-  set_y_rotation(d);
-  deg += 0.5;
-  if (deg > 360) deg = 0;
+  if (rotation_x != rotation_x_target) {
+    int step = 1;
+    if (rotation_x_target < rotation_x) step *= -1;
+    set_x_rotation(rotation_x + step);
+  }
+  if (rotation_y != rotation_y_target) {
+    int step = 1;
+    if (rotation_y_target < rotation_y) step *= -1;
+    set_y_rotation(rotation_y + step);
+  }
 }
 
-void set_x_rotation(double angle) {
-  angle = constrain(angle, -90, 90);
-  lcd_line(String(angle));
-  angle += 90;
-  double w = map(angle, 0, 180, SERVOMIN, SERVOMAX);
-  pwm.setPWM(0, 0, int(w));
+void set_x_rotation(int x) {
+  rotation_x = constrain(x, -90, 90);
+  // lcd_line(String(angle));
+  int angle = rotation_x + 90;
+  int w = map(angle, 0, 180, SERVOMIN, SERVOMAX);
+  pwm.setPWM(0, 0, w);
 }
 
-void set_y_rotation(double angle) {
-  angle = constrain(angle, 10, 60);
-  lcd_line(String(angle));
-  angle = 180 - angle;
-  double w = map(angle, 0, 180, SERVOMIN, SERVOMAX);
-  pwm.setPWM(1, 0, int(w));
-
+void set_y_rotation(double y) {
+  rotation_y = constrain(y, 10, 60);
+  lcd_line(String(rotation_y));
+  int angle = 180 - rotation_y;
+  int w = map(angle, 0, 180, SERVOMIN, SERVOMAX);
+  lcd_line(String(w));
+  pwm.setPWM(1, 0, w);
 }
 
 void loop() {
